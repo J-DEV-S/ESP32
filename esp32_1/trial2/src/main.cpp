edeaa14d41ea939ae602f01e088583b7c8e27c09
devesh <js.devesh2003@gmail.com>
2023-08-20 23:07:57 +0530

    Signed-off-by: devesh <js.devesh2003@gmail.com> BLINK PROGRAM FOR ESP32 IN PIO

---
+++ esp32_1/trial2/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+//extern "C"
+
+#define LED 1
+void setup()
+{
+  pinMode(LED,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
+}
